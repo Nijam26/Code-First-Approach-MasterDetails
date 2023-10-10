2199656a06b8ecc74c14bb26af8b7d722a344f4d
IDB _C# <IDB_C#@nh.ctg>
2023-10-10 14:40:13 +0600

    first commit

---
+++ DAL/OrderContext.cs
@@ -0,0 +1,28 @@
+﻿using My_Evidence.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace My_Evidence.DAL
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext() : base("Exam_DB")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderMaster> OrderMasters { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            Configuration.ProxyCreationEnabled = false;
+        }
+    }
+}
